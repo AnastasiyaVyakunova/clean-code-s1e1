7a5ff31313623723a9df23e56c638c063543edb0
AnastasiyaVyakunova <anastasiya.vyakunova@yandex.ru>
2021-11-27 18:12:12 +0300

    refactor: apply BEM notation according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -90,11 +90,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var containsClass=listItem.classList.contains("li_edit-mode");
+    //If class of the parent is .li_edit-mode
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .li_edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -103,8 +103,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .li_edit-mode on the parent.
+    listItem.classList.toggle("li_edit-mode");
 };
 
 
